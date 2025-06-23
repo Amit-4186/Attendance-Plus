f9eb5de8c228d9348d68e90e826a9c1340ba9314
Amit-4186 <amitkrpatel4.186@gmail.com>
2025-06-23 22:52:11 +0530

    SubjectScreen UI update

--- app/src/main/java/com/example/attendanceplus/screens/SubjectScreen.kt
+++ app/src/main/java/com/example/attendanceplus/screens/SubjectScreen.kt
@@ -1,12 +1,21 @@
 package com.example.attendanceplus.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Add
@@ -16,11 +25,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,11 +39,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.attendanceplus.Screen
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,10 +61,33 @@ fun SubjectsScreen(navController: NavController) {
     val viewModel: SetupViewModel = hiltViewModel()
     val subjects by viewModel.subjects.collectAsState()
     var showDialog by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+
+    var newSubject by remember { mutableStateOf("") }
+
+    fun addSubject() {
+        if (newSubject.isNotEmpty()) {
+            if (subjects.any { it.name == newSubject }) {
+                Toast.makeText(context, "Subject already exists!", Toast.LENGTH_SHORT).show()
+            } else {
+                viewModel.addSubject(newSubject)
+                showDialog = false
+                newSubject = ""
+            }
+        } else {
+            Toast.makeText(context, "Subject name can't be empty!", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Add Subjects") })
+            TopAppBar(title = {
+                Text(
+                    "Add Subjects",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            })
         },
         floatingActionButton = {
             Row {
@@ -53,8 +96,12 @@ fun SubjectsScreen(navController: NavController) {
                     modifier = Modifier.padding(4.dp)
                 ) {
                     Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text("Add Subjects", modifier = Modifier.padding(start = 8.dp))
-                        Icon(Icons.Filled.Add, "Add Subjects", modifier = Modifier.padding(end = 8.dp))
+                        Text("Add Subject", modifier = Modifier.padding(start = 8.dp))
+                        Icon(
+                            Icons.Filled.Add, "Add Subject", modifier = Modifier
+                                .padding(end = 8.dp)
+                                .size(16.dp)
+                        )
                     }
                 }
                 FloatingActionButton(
@@ -63,7 +110,11 @@ fun SubjectsScreen(navController: NavController) {
                 ) {
                     Row {
                         Text("Next", modifier = Modifier.padding(start = 8.dp))
-                        Icon(Icons.AutoMirrored.Filled.ArrowForward, "Next", modifier = Modifier.padding(end = 8.dp))
+                        Icon(
+                            Icons.AutoMirrored.Filled.ArrowForward, "Next", modifier = Modifier
+                                .padding(end = 8.dp)
+                                .size(16.dp)
+                        )
                     }
                 }
             }
@@ -73,24 +124,36 @@ fun SubjectsScreen(navController: NavController) {
         if (showDialog) {
             Dialog(onDismissRequest = { showDialog = false }) {
 
-                var newSubject by remember { mutableStateOf("") }
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .clip(RoundedCornerShape(8.dp))
+                ) {
                     OutlinedTextField(
                         value = newSubject,
-                        onValueChange = { newSubject = it }
+                        onValueChange = { newSubject = it },
+                        modifier = Modifier
+                            .padding(12.dp),
+                        label = { Text("Subject Name") },
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Done
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onDone = { addSubject() }
+                        )
                     )
-                    Row {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        modifier = Modifier
+                            .padding(bottom = 12.dp)
+                            .fillMaxWidth()
+                    ) {
                         Button(onClick = { showDialog = false }) {
                             Text("Cancel")
                         }
-                        Button(onClick = {
-                            if (newSubject.isNotEmpty()) {
-                                viewModel.addSubject(newSubject)
-                            } else {
-                                //TODO subject name cannot be empty
-                            }
-                            newSubject = ""
-                        }) {
+                        Button(onClick = { addSubject() }) {
                             Text("Add Subject")
                         }
                     }
@@ -98,16 +161,28 @@ fun SubjectsScreen(navController: NavController) {
             }
         }
 
-        LazyColumn(modifier = Modifier.padding(padding)) {
-
+        if (subjects.isEmpty()) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "No Subjects Added")
+            }
+        }
+        LazyColumn(modifier = Modifier.padding(12.dp)) {
             subjects.forEach { subject ->
                 item {
                     Row(
                         horizontalArrangement = Arrangement.SpaceBetween,
                         verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(4.dp)
+                            .background(
+                                MaterialTheme.colorScheme.primary,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .clip(RoundedCornerShape(8.dp))
+                            .height(60.dp)
                     ) {
-                        Text(subject.name)
+                        Text("subject.name", modifier = Modifier.padding(start = 12.dp), fontSize = 20.sp)
                         IconButton(onClick = { viewModel.deleteSubject(subject.id) }) {
                             Icon(Icons.Filled.Delete, "Delete")
                         }
